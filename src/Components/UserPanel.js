1d177871de62b5402309024bed78049b552796c5
niemiecjakub <kniemiec3@gmail.com>
2024-05-18 11:13:42 +0200

    3

--- src/Components/UserPanel.js
+++ src/Components/UserPanel.js
@@ -1,8 +1,13 @@
+import NewMeetingForm from "./NewMeetingForm";
+import MeetingsList from "./MeetingsList";
+import MeetingsPage from "./MeetingsPage";
+
 export default function UserPanel({username, handleLogout}) {
 
     return <div>
         <h1>Witaj {username}</h1>
         <button onClick={handleLogout}>Wyloguj</button>
+        <MeetingsPage />
     </div>;
 
 }
